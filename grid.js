788b6eb570391021f727d59f70cd0e8e215a0f43
lucafus <luca.fusaro1996@gmail.com>
2023-12-18 17:59:00 -0300

    Grid graphic interface and functions finished, problem wi th logic basics. I will have to change what value the user inputs

--- grid.js
+++ grid.js
@@ -1,25 +1,44 @@
+    const container = document.getElementById('grid');
+    const btnr = document.getElementById('reset');
+    const btne = document.getElementById('edit');
 
-const container = document.getElementById('container');
 
-const btnr = document.getElementById('reset');
+    btne.addEventListener("click", function() {
+
+    container.innerHTML = "";
+
+    let userinput = prompt("Type you size: ");
 
 
-for (let i = 1; i < 897; i++) {
-    const gridItem = document.createElement('div')
-    container.appendChild(gridItem);
-    gridItem.className = 'grid-item';
-    gridItem.addEventListener ("mouseenter",
-    (event) => {
-        event.target.style.backgroundColor = "red";
-            }
+
+
+    if (userinput > 0 && userinput <= 100) {alert(`We are going to create a grid of ${userinput} x ${userinput} `);}
+    else { alert("Please select a value between 100 and 1");
+    return;}
+
+
+
     
-            );
 
-    btnr.addEventListener("click", () => {
-        gridItem.style.backgroundColor = "white";
-        });
+        for (let i = 1; i < userinput * userinput; i++) {
+            const gridItem = document.createElement('div');
+            container.appendChild(gridItem);
+            gridItem.className = 'grid-item';
 
-     };
 
+
+            gridItem.addEventListener("mouseenter", (event) => {
+                event.target.style.backgroundColor = "red";
+            });
+
+            btnr.addEventListener("click", () => {
+                gridItem.style.backgroundColor = "white";
+            });
+                
+
+        };
+
+        
+    });
 
 
