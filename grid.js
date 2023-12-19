17ba1f45444e9a0427eecc1b58d67911744b1e4f
lucafus <luca.fusaro1996@gmail.com>
2023-12-19 17:25:46 -0300

    Grid logic completed, color buttons added, project logic and functionality completed

--- grid.js
+++ grid.js
@@ -1,41 +1,55 @@
-    const container = document.getElementById('grid');
+    const grid = document.getElementById('grid');
     const btnr = document.getElementById('reset');
     const btne = document.getElementById('edit');
+    const btnb = document.getElementById('black');
+    const btnred = document.getElementById('red');
 
 
     btne.addEventListener("click", function() {
 
-    container.innerHTML = "";
+    grid.innerHTML = "";
 
     let userinput = prompt("Type you size: ");
-
-
 
 
     if (userinput > 0 && userinput <= 100) {alert(`We are going to create a grid of ${userinput} x ${userinput} `);}
     else { alert("Please select a value between 100 and 1");
     return;}
 
-
-
-    
+   
 
         for (let i = 1; i < userinput * userinput; i++) {
             const gridItem = document.createElement('div');
-            container.appendChild(gridItem);
+            grid.appendChild(gridItem);
             gridItem.className = 'grid-item';
 
-
-
+            const itemSizePercentage = 100 / userinput;
+            gridItem.style.width = `calc(${itemSizePercentage}%)`; // Consider border
+            gridItem.style.height = `calc(${itemSizePercentage}%)`; // Consider border
+  
             gridItem.addEventListener("mouseenter", (event) => {
                 event.target.style.backgroundColor = "red";
             });
 
+
+            btnred.addEventListener("click", () => {
+                gridItem.addEventListener("mouseenter", (event) => {
+                    event.target.style.backgroundColor = "red";
+                });
+            });
+         
+
             btnr.addEventListener("click", () => {
                 gridItem.style.backgroundColor = "white";
             });
-                
 
+
+            btnb.addEventListener("click", () => {
+                gridItem.addEventListener("mouseenter", (event) => {
+                    event.target.style.backgroundColor = "black";
+                });
+            });
+         
         };
 
         
